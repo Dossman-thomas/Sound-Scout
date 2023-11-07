86ccdb1737ea0655d7295a1d2353967ca93f40d6
Utero93 <nirvanautero93@gmail.com>
2023-11-07 16:31:26 -0500

    nicks new code

--- assets/index.js
+++ assets/index.js
@@ -75,3 +75,20 @@ async function rapidData() {
 }
 
 // rapidData();
+
+const url = 'https://shazam.p.rapidapi.com/songs/list-artist-top-tracks?id=40008598&locale=en-US';
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '2df4822ac5msh9ecae0cf2c6416ep158190jsn26b3f314891f',
+        'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
+    }
+};
+
+try {
+    const response = await fetch(url, options);
+    const result = await response.text();
+    // console.log(result);
+} catch (error) {
+    console.error(error);
+}
